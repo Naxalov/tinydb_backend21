9692f6023eb011fe39c073d282999eb277b4fcf1
ZARIFMAC <uzbzarif@gmail.com>
2021-02-14 21:18:24 +0900

    insert multiply

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import requests
-print('hello')
+from tinydb import TinyDB
+db = TinyDB('db.json')
+# Document
+user1 = {'user_id':1,'username':'username1'}
+user2 = {'user_id':2,'username':'username3'}
+user3 = {'user_id':3,'username':'username6'}
+
+# db.insert(user1)
+# db.insert(user2)
+# db.insert(user3)
+
+db.insert_multiple([user1,user2])
