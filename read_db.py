499b47087e963fdaa93819dc01ebbcc2c9c2c0e3
ZARIFMAC <uzbzarif@gmail.com>
2021-02-14 21:37:09 +0900

    remove

--- read_db.py
+++ read_db.py
@@ -2,11 +2,14 @@ from tinydb import TinyDB,Query
 db = TinyDB('db.json')
 # Document
 # db.truncate()
-data = db.all()
 
 q = Query()
-user = db.search(q.user_id>2)
-print(user)
-# for i in data:
-#     print(i['user_id'])
-#     print(i['username'])
+db.remove(q.user_id==4)
+
+data = db.all()
+# user = db.search(q.user_id>2)
+# print(user)
+
+for i in data:
+    print(i['user_id'])
+    print(i['username'])
