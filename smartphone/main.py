ee4c751b448fc4fcd47f02961900c812aad78749
ZARIFMAC <uzbzarif@gmail.com>
2021-02-17 21:44:17 +0900

    insert

--- smartphone/main.py
+++ smartphone/main.py
@@ -1,4 +1,14 @@
+from tinydb import TinyDB
+
+db = TinyDB('db.json')
 products = open('products.csv').read().split('\n')
 # print(products)
-for row in products:
-    print(row)
+db.truncate()
+products_db = []
+for row in products[1:]:
+    cat,company=row.split(',')[1:]
+    db.insert({'category':cat,'company':company})
+    
+
+# db.insert({'category':'smartphone','company':'Samsung'})
+
